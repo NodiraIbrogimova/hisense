c4e69e2220f97db1a9dd8e16cc11f06ff73acd46
Nodira Ibragimova <nodira.ibrogimova@gmail.com>
2022-08-06 01:40:00 +0500

    fix: awards pages text size on mobila & add clockdown

--- src/app/pages/awards/awards.component.ts
+++ src/app/pages/awards/awards.component.ts
@@ -8,6 +8,14 @@ import { judges, orgCommittee } from './variables';
   styleUrls: ['./awards.component.scss']
 })
 export class AwardsComponent implements OnInit {
+  deadline = new Date('September 17 2022 23:59:59 GMT+0500');
+  // deadline = new Date('August 6 2022 1:28:59 GMT+0500');
+
+  applicationIsClosed: boolean = false;
+  seconds: any = 0;
+  minutes: any = 0;
+  hours: any = 0;
+  days: any = 0;
   navItems = [
     {id: 1, name: 'О премии', tabLink: 'about-awards'},
     {id: 2, name: 'Призы и сроки', tabLink: 'awards-and-deadlines'},
@@ -21,7 +29,14 @@ export class AwardsComponent implements OnInit {
 
   constructor(@Inject(DOCUMENT) private document: Document) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.calculateClockInterval;
+  }
+
+  calculateClockInterval = setInterval(() => {
+      this.getTimeRemaining(this.deadline);
+  }, 1000);
+  
 
   activatePage(pageName: string){
     const id = pageName;
@@ -30,6 +45,30 @@ export class AwardsComponent implements OnInit {
 
   openPage(pageName: any){
     return pageName === this.activeLink ? 'visible' : 'hidden';
+  }
+
+  getTimeRemaining(endtime: Date){
+      const today = new Date().getTime();
+      const total = endtime.getTime() - today;
+      this.seconds = Math.floor( (total/1000) % 60 );
+      this.minutes = Math.floor( (total/1000/60) % 60 );
+      this.hours = Math.floor( (total/(1000*60*60)) % 24 );
+      this.days = Math.floor( total/(1000*60*60*24) );
+    
+      if (total <= 0) {
+        clearInterval(this.calculateClockInterval);
+        this.applicationIsClosed = true;
+        this.days = '00';
+        this.hours = '00';
+        this.minutes = '00';
+        this.seconds = '00';
+      }
+
+      this.seconds = ('0' + this.seconds).slice(-2);
+      this.minutes = ('0' + this.minutes).slice(-2);
+      this.hours = ('0' + this.hours).slice(-2);
+      this.days = ('0' + this.days).slice(-2);
+      console.log(`time is: ${total}`);
   }
 
 }
